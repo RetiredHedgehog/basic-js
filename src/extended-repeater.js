31f57002129ab6640b6bf554fbb50720d6da84bc
Nikita Lukashov <56773398+RetiredHedgehog@users.noreply.github.com>
2021-03-01 13:22:25 +0300

    feat: adde solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  function repeaterAdditional (addition = '', additionRepeatTimes = 1, additionSeparator = '|') {
+    let temp = '';
+    
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      temp += addition + additionSeparator;
+    }
+
+    return temp.slice(0, -(additionSeparator.length));
+  }
+
+  function repeaterStr (str = '', repeatTimes = 1, separator = '+') {
+    let temp = '';
+
+    for (let i = 0; i < repeatTimes; i++) {
+      temp += str + repeaterAdditional(options.addition, options.additionRepeatTimes, options.additionSeparator) + separator;
+    }
+    
+    return temp.slice(0, -(separator.length));
+  }
+
+  return repeaterStr(str, options.repeatTimes, options.separator);
 };
   
