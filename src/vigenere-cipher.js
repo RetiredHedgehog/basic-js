c3dd32799cbb09b4b362452d37e56a4ee7095358
Nikita Lukashov <56773398+RetiredHedgehog@users.noreply.github.com>
2021-03-05 20:22:57 +0300

    feat: added solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,63 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(bool = true) {
+    this.bool = bool;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+  encrypt(message, key) {
+    if ( message === undefined || key === undefined ) {
+      throw new Error
+    }
+
+    if ( message.length !== key.length ) {
+      key = key.repeat(Math.ceil(message.length/key.length)).slice(0, message.length);
+    }
+    
+    let messageEnctypted = '';
+    
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    for (let i = 0; i < message.length; i++) {
+      if ( this.alphabet.indexOf(message[i]) === -1 ) {
+        messageEnctypted += message[i];
+        message = message.slice(0, i) + message.slice(i + 1);
+        i--;
+      }
+      else {
+        messageEnctypted += this.alphabet[(this.alphabet.indexOf(message[i]) + this.alphabet.indexOf(key[i])) % this.alphabet.length];
+      }
+    }
+    
+    return this.bool === true ? messageEnctypted : [...messageEnctypted].reverse().join('');
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(encryptedMessage, key) {
+    if ( encryptedMessage  === undefined || key === undefined ) {
+      throw new Error
+    }
+
+    if ( encryptedMessage.length !== key.length ) {
+      key = key.repeat(Math.ceil(encryptedMessage.length/key.length)).slice(0, encryptedMessage.length);
+    }
+    
+    let messageDectypted = '';
+    
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if ( this.alphabet.indexOf(encryptedMessage[i]) === -1 ) {
+        messageDectypted += encryptedMessage[i];
+        encryptedMessage = encryptedMessage.slice(0, i) + encryptedMessage.slice(i + 1);
+        i--;
+      }
+      else {
+        messageDectypted += this.alphabet[(this.alphabet.indexOf(encryptedMessage[i]) - this.alphabet.indexOf(key[i]) + this.alphabet.length) % this.alphabet.length];
+      }
+    }
+    
+    return this.bool === true ? messageDectypted : [...messageDectypted].reverse().join('');
   }
 }
 
