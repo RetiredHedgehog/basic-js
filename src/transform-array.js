613515fef77737d36823772180b7bdff7f895b61
Nikita Lukashov <56773398+RetiredHedgehog@users.noreply.github.com>
2021-03-05 20:22:12 +0300

    feat: added solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if ( !Array.isArray(arr) ) {
+    throw new Error;
+  }
+
+  let control = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+  let arrCopy = arr.slice();
+  let arrT = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if ( arrCopy[i] === control[0] ) {
+      arrCopy[i + 1] = undefined;
+      continue;
+    }
+    if ( arrCopy[i] === control[1] && typeof arrCopy[i-1] !== 'undefined' ) {
+      arrCopy[i - 1] = undefined;
+      arrT.pop();
+      continue;
+    }
+    if ( arrCopy[i] === control[2] && typeof arrCopy[i+1] !== 'undefined' ) {
+      arrT.push(arrCopy[i + 1]);
+      continue;
+    }
+    if ( arrCopy[i] === control[3] && typeof arrCopy[i-1] !== 'undefined' ) {
+      arrT.push(arrCopy[i - 1]);
+      continue;
+    }
+    if ( typeof arrCopy[i] !== 'undefined' &&  control.indexOf(arrCopy[i]) === -1 ) {
+      arrT.push(arrCopy[i])  
+      continue;
+    }
+  }
+  return arrT;
 };
